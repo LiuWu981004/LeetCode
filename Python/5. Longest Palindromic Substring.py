cd511304183b1f1098cf75497b64b739abfed58f
LiuWu981004 <36839007+LiuWu981004@users.noreply.github.com>
2019-07-30 19:31:51 +0800

    Update 5. Longest Palindromic Substring.py

--- Python/5. Longest Palindromic Substring.py
+++ Python/5. Longest Palindromic Substring.py
@@ -1,3 +1,19 @@
 class Solution:
   def longestPalindrome(self,s):
     palindrome = ''
+    for i in range(len(s)):  # range产生的数字是从0 到len - 1 正好与字符串索引匹配
+      len1 = self.getlongestpalindrome(s,i,i) #aba 型回文判断
+      if len(len1) > len(palindrome):
+        palindrome = len1
+      len2 = self.getlongestpalindrome(s,i,i+1) # aa型回文判断
+      if len(len2) > len(palindrome):
+        palindrome = len2
+     return palindrome
+      
+    
+  def getlongestpalindrome(self,s,l,r):
+    #找出回文片段
+    while l >=0 and r < len(s) and s[l] == s[r]:  #回文条件
+      l -= 1
+      r += 1
+     return s[l+1:r] # 此时 l和r都进行了数值操作 而且与索引匹配， 左开右闭，l则需加一，r无需操作。
